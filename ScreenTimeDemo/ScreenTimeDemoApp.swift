5e520ca43446383ba9fb9bb03c32137f6bdbe946
David Osemwegie <35180584+davidosemwegie@users.noreply.github.com>
2024-01-21 15:19:03 -0700

    Initial Commit

---
+++ ScreenTimeDemo/ScreenTimeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTimeDemoApp.swift
+//  ScreenTimeDemo
+//
+//  Created by David Osemwegie on 2024-01-21.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTimeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
